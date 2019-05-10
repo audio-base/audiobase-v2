ea7a5799f707dcb92c44f4dc46434e869e77baa5
Daniel Su <danielsu@protonmail.ch>
2019-05-09 20:52:45 -0700

    [tweaks] minor changes

--- example_config.js
+++ example_config.js
@@ -1,3 +1,17 @@
+import Firebase from 'firebase';
+
 const SC_KEY = 'API_KEY';
 
-module.exports = SC_KEY;
+const firebaseConfig = {
+  apiKey: 'API_KEY',
+  authDomain: 'audiobase-chat.firebaseapp.com',
+  databaseURL: 'https://audiobase-chat.firebaseio.com',
+  projectId: 'audiobase-chat',
+  storageBucket: 'audiobase-chat.appspot.com',
+  messagingSenderId: '587502341898',
+  appId: '1:587502341898:web:c9901f44f9da377a',
+};
+const app = Firebase.initializeApp(firebaseConfig);
+const db = app.database();
+
+module.exports = { SC_KEY, db, firebaseConfig };
